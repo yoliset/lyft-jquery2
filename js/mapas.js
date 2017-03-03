5b4c56a6e13b3f9ae4ee276682a4e3ea6262ebe4
yolisett <lizbeth9589@gmail.com>
2017-03-03 04:21:44 -0500

    First commit

---
+++ js/mapas.js
@@ -0,0 +1,45 @@
+    
+var map;
+function initMap() {
+  var myLatLng = {lat: -16.457389199999998, lng: -71.5315308};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: myLatLng,
+         mapTypeControl: true,
+         mapTypeControlOptions: {
+              style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+              position: google.maps.ControlPosition.TOP_CENTER
+    },
+         zoomControl: true,
+         zoomControlOptions: {
+              position: google.maps.ControlPosition.LEFT_CENTER
+    },
+         scaleControl: true,
+         streetViewControl: true,
+         streetViewControlOptions: {
+              position: google.maps.ControlPosition.LEFT_TOP
+    }
+  });  
+
+  var marker = new google.maps.Marker({
+         position: myLatLng,
+         map: map,
+         title: 'Hello World!'
+  });
+    
+  var image = '/img/nia.png';
+  var beachMarker = new google.maps.Marker({
+         position: {lat: -16.457389199999998, lng: -71.5315301},
+         map: map,
+         icon: image
+  });
+   var image = '/img/nia.png';
+  var beachMarker = new google.maps.Marker({
+         position: {lat: -16.457389199999998, lng: -71.5315301},
+         map: map,
+         icon: image
+  });
+}
+
+
