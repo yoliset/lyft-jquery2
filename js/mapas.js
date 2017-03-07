031b317542cc565b31a40bc9bf803c1a1079d30b
yolisett <lizbeth9589@gmail.com>
2017-03-06 21:34:01 -0500

    Arreglo en html

--- js/mapas.js
+++ js/mapas.js
@@ -1,60 +1,38 @@
     
-var map;
 function initMap() {
-  var myLatLng = {lat: -16.457389199999998, lng: -71.5315308};
-
-  var map = new google.maps.Map(document.getElementById('map'), {
+    var myLatLng = {lat: -16.457148 , lng: -71.530759};
+    var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 16,
     center: myLatLng,
-         mapTypeControl: true,
-         mapTypeControlOptions: {
-              style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
-              position: google.maps.ControlPosition.TOP_CENTER
-    },
-         zoomControl: true,
-         zoomControlOptions: {
-              position: google.maps.ControlPosition.LEFT_CENTER
-    },
-         scaleControl: true,
-         streetViewControl: true,
-         streetViewControlOptions: {
-              position: google.maps.ControlPosition.LEFT_TOP
-    }
-  });  
-
-  var marker = new google.maps.Marker({
-         position: myLatLng,
-         map: map,
-         title: 'Aqui estoy!'
+    disableDefaultUI: true
+  });
+    var myLatLng = {lat: -16.457148 , lng: -71.530759};
+    var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Aqui estoy',
+  });
+    var myLatLng1 = {lat: -16.459128, lng: -71.525970};
+    var marker = new google.maps.Marker({
+    position: myLatLng1,
+    map: map,
+    title: 'Miguel',
+    icon: "/img/car1.png"
+  });
+    var myLatLng2 = {lat: -16.455101 , lng: -71.529467};
+    var marker = new google.maps.Marker({
+    position: myLatLng2,
+    map: map,
+    title: 'Mirrela',
+    icon: "/img/car2.png"
   });
    
-    var myLatLng1 =  {lat: -16.455348 , lng: -71.532789};
-    var marker = new google.maps.Marker({
-    position:myLatLng1,
-    map: map,
-    title: 'Sandra',
-    icon: '/img/car1.png'
-  });
-    var myLatLng2 = {lat: -16.459128, lng: -71.529870};
-    var marker = new google.maps.Marker({
-    position:myLatLng2,
-    map: map,
-    title: 'Julio',
-    icon: '/img/car2.png'
-  })
-    var myLatLng3 = {lat: -16.455688 , lng: -71.525989};
-    var marker = new google.maps.Marker({
-    position: myLatLng3,
-    map: map,
-    title: 'Sandro',
-    icon: '/img/car3.png'
-  });
-     var myLatLng4 = {lat: -16.455648 , lng: -71.535870};
+    var myLatLng4 = {lat: -16.453259 , lng: -71.530539};
     var marker = new google.maps.Marker({
     position: myLatLng4,
     map: map,
-    title: 'Susan',
-    icon: '/img/car4.png'
+    title: 'Helen',
+    icon: "/img/car1.png"
   });
     var myLatLng5 =  {lat: -16.455101 , lng: -71.529467};
     var marker = new google.maps.Marker({
@@ -63,7 +41,15 @@ function initMap() {
     title: 'Jorge',
     icon: "/img/car2.png"
   });
+     var myLatLng6 = {lat: -16.455348 , lng: -71.532789};
+    var marker = new google.maps.Marker({
+    position: myLatLng6,
+    map: map,
+    title: 'Josue',
+    icon: "/img/car3.png"
+  });
 }
+
 function validateList () {
     var selec_Lista = $("li").on("click");
     if (selec_Lista ==0)
