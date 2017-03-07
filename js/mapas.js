38429bee209cfe095074bbd9e8937328ef4f1ce6
yolisett <lizbeth9589@gmail.com>
2017-03-06 21:07:01 -0500

    Terminado

--- js/mapas.js
+++ js/mapas.js
@@ -4,7 +4,7 @@ function initMap() {
   var myLatLng = {lat: -16.457389199999998, lng: -71.5315308};
 
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 15,
+    zoom: 16,
     center: myLatLng,
          mapTypeControl: true,
          mapTypeControlOptions: {
@@ -25,21 +25,50 @@ function initMap() {
   var marker = new google.maps.Marker({
          position: myLatLng,
          map: map,
-         title: 'Hello World!'
+         title: 'Aqui estoy!'
   });
-    
-  var image = '/img/nia.png';
-  var beachMarker = new google.maps.Marker({
-         position: {lat: -16.457389199999998, lng: -71.5315301},
-         map: map,
-         icon: image
+   
+    var myLatLng1 =  {lat: -16.455348 , lng: -71.532789};
+    var marker = new google.maps.Marker({
+    position:myLatLng1,
+    map: map,
+    title: 'Sandra',
+    icon: '/img/car1.png'
   });
-   var image = '/img/nia.png';
-  var beachMarker = new google.maps.Marker({
-         position: {lat: -16.457389199999998, lng: -71.5315301},
-         map: map,
-         icon: image
+    var myLatLng2 = {lat: -16.459128, lng: -71.529870};
+    var marker = new google.maps.Marker({
+    position:myLatLng2,
+    map: map,
+    title: 'Julio',
+    icon: '/img/car2.png'
+  })
+    var myLatLng3 = {lat: -16.455688 , lng: -71.525989};
+    var marker = new google.maps.Marker({
+    position: myLatLng3,
+    map: map,
+    title: 'Sandro',
+    icon: '/img/car3.png'
+  });
+     var myLatLng4 = {lat: -16.455648 , lng: -71.535870};
+    var marker = new google.maps.Marker({
+    position: myLatLng4,
+    map: map,
+    title: 'Susan',
+    icon: '/img/car4.png'
+  });
+    var myLatLng5 =  {lat: -16.455101 , lng: -71.529467};
+    var marker = new google.maps.Marker({
+    position: myLatLng5,
+    map: map,
+    title: 'Jorge',
+    icon: "/img/car2.png"
   });
 }
-
-
+function validateList () {
+    var selec_Lista = $("li").on("click");
+    if (selec_Lista ==0)
+       alert("Escoge una opción");
+    else {
+         window.location.href = 'mapa-2.html';
+    }
+}
